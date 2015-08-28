de6a0280ce67a8acff4d1727df596c8c71b308d8
Beerend Lauwers <beerend.lauwers@capgemini.com>
2015-08-28 18:55:14 +0200

    Cleaned up metadata element

--- app/js/metadata.js
+++ app/js/metadata.js
@@ -1,17 +1,31 @@
-$(document).ready(function() {   
-    console.log("test");
-    $.ajax( {
-            url: "../../json/tags.json", // TODO: find a fix?
-            type: "GET",
-            dataType: "jsonp"
-        }) // TODO: could we fix this ?
-        .done( function(msg) {
-            console.log("success");
-            console.log(msg);
+function loadDropdowns() {
+var dropdowns = [
+        ["../../json/tags.json",'#tags'],
+        ["../../json/libraries.json",'#libraries'],
+        ];
+
+    R.map ( R.apply(loadMetaDataDropdown), dropdowns );
+}
+
+function loadMetaDataDropdown( url, id ) {
+    $.ajax( url ) // TODO: find a fix?
+        .done( function(data) {
+            var options = R.map( function(item) { return '<option value="' + item + '">' + item + '</option>'; } )( data );
+            $(id).append(options);
+            $(id).chosen({ width: '100%' });
+            $(".chosen-container-multi").find("input").on('keydown', function (evt) {
+                var stroke;
+                stroke = (_ref = evt.which) != null ? _ref : evt.keyCode;
+                if (stroke == 9) { // 9 = tab key
+                    $(id).append('<option value="' + $(this).val() + '" selected="selected">' + $(this).val() + '</option>');
+                    $(id).trigger('chosen:updated');
+                    evt.preventDefault();
+                }
+            });
         }).fail(function(xhr, ajaxOptions, thrownError) {
             console.log("failure");
             console.log(xhr);
             console.log(ajaxOptions);
             console.log(thrownError);
         });
-});
+}
