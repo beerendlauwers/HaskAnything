53bb94e504465ed6a31b07f93d4f82ab511953d0
Beerend Lauwers <beerend.lauwers@capgemini.com>
2016-02-09 20:32:16 +0100

    More work on the Github connection stuff.

--- app/js/generate-file-preview.js
+++ app/js/generate-file-preview.js
@@ -2,7 +2,7 @@
 function generateFilePreview(templateName) {
     
     // Fetch elements
-    var inputSelector = jQuery(':input');
+    var inputSelector = jQuery(':input').not('#submit-pull-request-preview, #title-proposed, #submit-pull-request-feedback');
     
     var allValues = inputSelector.map(function() { return jQuery(this).val(); } );
     var allNames = inputSelector.map(function() { return jQuery(this).attr('id'); } );
@@ -24,6 +24,24 @@ function generateFilePreview(templateName) {
     return preview;
 }
 
-function displayFilePreview(templateName,selector) {
-    jQuery(selector).text( generateFilePreview(templateName) );
+function displayFilePreview(templateName,fileSelector,titleSelector) {
+    jQuery(fileSelector).text( generateFilePreview(templateName) );
+    jQuery(titleSelector).val( generateFileTitle(templateName) );
+}
+
+function generateFileTitle(templateName) {
+    var str = jQuery('.title-suggestion').val();
+    return templateName + "-" + str.replace(/\W+/g, '-').toLowerCase() + ".md";
+}
+
+function getFinalFileTitle(templateName, titleSelector) {
+    var proposedTitle = generateFileTitle(templateName);
+    
+    var possiblyChangedTitle = jQuery(titleSelector).val();
+    
+    if (possiblyChangedTitle.trim() && possiblyChangedTitle !== proposedTitle) {
+        return possiblyChangedTitle;
+    }
+    
+    return proposedTitle;
 }
