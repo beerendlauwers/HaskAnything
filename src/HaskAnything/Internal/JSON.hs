97a2026fa57bea8de5f32e23eaf54ed5f31547cd
Beerend Lauwers <beerend.lauwers@capgemini.com>
2016-03-11 15:55:50 +0100

    JSON code modified

--- src/HaskAnything/Internal/JSON.hs
+++ src/HaskAnything/Internal/JSON.hs
@@ -7,8 +7,10 @@ import           Hakyll
 import           Data.Aeson                      (encode)
 import           HaskAnything.Internal.Tags      (getUniqueTags')
 
-tagsToJSON name tags = do
+makeJSONFile name tags = do
     create [fromFilePath ("json/" ++ name ++ ".json")] $ do
         route idRoute
         compile $ do
-            makeItem (encode $ getUniqueTags' tags)
+            makeItem (tagsToJSON tags)
+            
+tagsToJSON tags = encode $ getUniqueTags' tags
