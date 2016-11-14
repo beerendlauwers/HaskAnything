93af933a98ac35a27c4327206b982639e14d9393
Beerend Lauwers <beerend.lauwers@capgemini.com>
2016-11-14 15:41:41 +0100

    Removed old unused code

--- src/HaskAnything/Internal/Field.hs
+++ src/HaskAnything/Internal/Field.hs
@@ -6,56 +6,9 @@ import           Hakyll
 import           Data.Data
 import           Data.String.Utils             (replace)
 
-urlReplaceField :: String -> (String,String) -> Context a 
-urlReplaceField fieldName (old,new) = field fieldName $ \item -> do 
-        mbFilePath <- getRoute (itemIdentifier item) 
-        case mbFilePath of 
-            Nothing       -> return "urlReplaceField: ???" 
+urlReplaceField :: String -> (String,String) -> Context a
+urlReplaceField fieldName (old,new) = field fieldName $ \item -> do
+        mbFilePath <- getRoute (itemIdentifier item)
+        case mbFilePath of
+            Nothing       -> return "urlReplaceField: ???"
             Just filePath -> return $ toUrl $ replace old new $ filePath 
-
-class HakyllField a where
-    toHakyllField :: String -> a -> Context b
-    
-instance HakyllField Int where
-    toHakyllField key = field key . const . return . show
-    
-instance HakyllField String where
-    toHakyllField = constField
-    
-data MyTestData = Testing { greetings :: String, testingthis :: Int } deriving (Data,Typeable)
-
--- Prelude Data.Data> constrFields (toConstr (Testing undefined undefined))
--- ["greetings","testingthis"]
-
-{-
-
-deriveListField :: String -> a -> Context b
-deriveListField listFieldName a = 
- let fields =  constrFields (toConstr a)
- in listField listFieldName (generateFields fields)
-  where
-    generateFields fields = foldr mappend missingField (map generateField fields)
-    generateField f = field f (return . -- Stuck here. How do we cast the field string to, well, an actual field without using TH?
- 
- -}
-
-{- 
-listField "facetList"
-                                (
-                                    field "facetName" (return . facetName . itemBody) <>
-                                    field "facetList" (return . facetList . itemBody) <>
-                                    field "facetPrettyName" (return . facetPrettyName . itemBody) <>
-                                    field "facetPath" (return . facetPath . itemBody)
-                                )
-                                (sequence $ map (\(nm,p,t) -> makeItem $ generateFacetList nm p t) [("Tags","tags",tags),("Categories","categories",categories),("Libraries","libraries",libraries)]) <>
-                   
-                   -}
- 
---instance HakyllField MyTestData where
---    toHakyllField = 
-     
-     
-
-    
---instance HakyllField a => HakyllField [a] where
---    toHakyllField (x:xs) = listFieldWith -}
