bfa682ab9c52bf191ea4941cd51e5320f0b901ec
Beerend Lauwers <beerend.lauwers@capgemini.com>
2016-11-15 09:14:16 +0100

    Added a function to load items from a metadata list and make them available as a Listfield

--- src/HaskAnything/Internal/Field.hs
+++ src/HaskAnything/Internal/Field.hs
@@ -4,11 +4,30 @@ module HaskAnything.Internal.Field where
 
 import           Hakyll
 import           Data.Data
+import           Data.Maybe                    (fromMaybe)
 import           Data.String.Utils             (replace)
+import           Control.Applicative           (empty)
 
 urlReplaceField :: String -> (String,String) -> Context a
 urlReplaceField fieldName (old,new) = field fieldName $ \item -> do
         mbFilePath <- getRoute (itemIdentifier item)
         case mbFilePath of
             Nothing       -> return "urlReplaceField: ???"
-            Just filePath -> return $ toUrl $ replace old new $ filePath 
+            Just filePath -> return $ toUrl $ replace old new $ filePath
+
+loadSeriesList :: Context String -> Context b
+loadSeriesList c = Context $ \k _ i ->
+  if k == "seriesList"
+    then
+      do
+        -- Load the metadata from the item.
+        metadata <- getMetadata (itemIdentifier i)
+        -- Get the values under the "list" key as a list of strings.
+        let listItems = (fromMaybe [] (lookupStringList "list" metadata)) :: [String]
+        -- Turn those strings into a list of valid Identifiers.
+        let filePaths = (map (\p -> fromFilePath ("content/" ++ p)) listItems) :: [Identifier]
+        -- Use the list of Identifiers to load them up as Items.
+        loadedItems <- (mapM load filePaths) :: Compiler [Item String]
+        -- Return a listField with the key "loadedItems" and loadedItems.
+        return (ListField c loadedItems)
+    else empty
