b2364abd14b14b573aed8337585fbcb7fcaa26bd
Beerend Lauwers <beerend.lauwers@capgemini.com>
2016-12-02 08:17:37 +0100

    Moved over getCategory to HaskAnything.Internal.Extra

--- src/HaskAnything/Internal/Extra.hs
+++ src/HaskAnything/Internal/Extra.hs
@@ -6,6 +6,7 @@ import           Hakyll
 
 import           Data.Char                       (chr)
 import           Data.ByteString.Lazy            (unpack,ByteString)
+import           System.FilePath                (takeBaseName, takeDirectory)
 
 -- Lazy bytestring stuff
 loadBodyLBS :: Identifier -> Compiler ByteString
@@ -14,3 +15,7 @@ loadBodyLBS = loadBody
 -- |Convert a lazy ByteString to a string without double quotes screwing things up. ASCII only.
 toString :: ByteString -> String
 toString = map (chr . fromEnum) . unpack
+
+-- Copied over from Hakyll.Web.Tags
+getCategory :: MonadMetadata m => Identifier -> m [String]
+getCategory = return . return . takeBaseName . takeDirectory . toFilePath
