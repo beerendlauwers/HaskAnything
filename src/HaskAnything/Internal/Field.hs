54a76a91103b4c1acb7410450da16669bab23290
Beerend Lauwers <beerend.lauwers@capgemini.com>
2016-12-02 08:18:17 +0100

    Updated loadSeriesList to select the appropriate context for its items. Unfortunately, we cannot have multiple contexts for a single list. Will look into this. + Refactoring

--- src/HaskAnything/Internal/Field.hs
+++ src/HaskAnything/Internal/Field.hs
@@ -4,9 +4,11 @@ module HaskAnything.Internal.Field where
 
 import           Hakyll
 import           Data.Data
+import           Data.List                     (find)
 import           Data.Maybe                    (fromMaybe)
 import           Data.String.Utils             (replace)
 import           Control.Applicative           (empty)
+import           HaskAnything.Internal.Extra   (getCategory)
 
 urlReplaceField :: String -> (String,String) -> Context a
 urlReplaceField fieldName (old,new) = field fieldName $ \item -> do
@@ -15,8 +17,14 @@ urlReplaceField fieldName (old,new) = field fieldName $ \item -> do
             Nothing       -> return "urlReplaceField: ???"
             Just filePath -> return $ toUrl $ replace old new $ filePath
 
-loadSeriesList :: Context String -> Context b
-loadSeriesList c = Context $ \k _ i ->
+getFieldFromMetadata :: String -> Context String
+getFieldFromMetadata key = field key (\i -> fmap (maybe empty id) (getMetadataField  (itemIdentifier i) key) )
+
+getManyFieldsFromMetaData :: [String] -> Context String
+getManyFieldsFromMetaData keys = foldr1 mappend (map getFieldFromMetadata keys)
+
+loadSeriesList :: [(String,Context String)] -> Context b
+loadSeriesList contextMap = Context $ \k _ i ->
   if k == "seriesList"
     then
       do
@@ -28,6 +36,15 @@ loadSeriesList c = Context $ \k _ i ->
         let filePaths = (map (\p -> fromFilePath ("content/" ++ p)) listItems) :: [Identifier]
         -- Use the list of Identifiers to load them up as Items.
         loadedItems <- (mapM load filePaths) :: Compiler [Item String]
+        -- Fetch the category of the first item. We will use that in our
+        -- ListField constructor. Will have to work something out for
+        -- ListFields with multiple context support.
+        categories <- mapM (getCategory) filePaths
         -- Return a listField with the key "loadedItems" and loadedItems.
-        return (ListField c loadedItems)
+        return (ListField (getContext ((head.head) categories)) loadedItems)
     else empty
+ where
+   getContext cat =
+     case find ((== cat) . fst) contextMap of
+       (Just c) -> snd c
+       otherwise -> error ("Could not find a context for category '" ++ cat ++ "' in loadSeriesList.")
