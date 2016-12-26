679d9ba215386e50a9f37729c2c002b3e41ca052
Beerend Lauwers <beerend.lauwers@capgemini.com>
2016-12-26 14:28:57 +0100

    Presentations, papers and articles (WIP) now support Markdown

--- app/js/generate-file-preview.js
+++ app/js/generate-file-preview.js
@@ -1,40 +1,40 @@
 // Fetch the form elements and funnel them into a handlebars template to get the final source file.
 function generateFilePreview(templateName) {
-    
+
     // Fetch elements
-    var inputSelector = jQuery('.web-submit-element').find(':input').not('.select2-search__field');
-    
+    var inputSelector = jQuery('.web-submit-element').find(':input').not('.select2-search__field').not('.btn-default').not('.btn');
+
     var selectors = jQuery(inputSelector).toArray();
-    
+
     var getValue = function(selector) {
         var v = jQuery(selector).val();
-        
+
         console.log("selector " + selector.id + ": " + v);
-        
+
         return v ? v : "";
     };
-    
+
     var allValues = [];
     jQuery(inputSelector).each( function( idx ) {
         var v = getValue(this);
         allValues.push( v );
     });
-    
+
     var allNames = inputSelector.map(function() { return jQuery(this).attr('id'); } );
-    
+
     // Turn into an object
     var zipped = R.zip( allNames, allValues );
 
     var objs = R.map( function(item) { var obj = {}; obj[item[0]] = item[1]; return obj; }, zipped);
-    
+
     var context = R.reduce( jQuery.extend, {}, objs );
 
     // Handlebars
     var source = jQuery("#handlebars-template").text();
     var template = Handlebars.compile(source);
-    
+
     var preview = template(context);
-    
+
     // Return
     return preview;
 }
@@ -51,15 +51,15 @@ function setAvailabilityPullRequest( value ) {
 
 function getFinalFileTitle(templateName, titleSelector) {
     var proposedTitle = generateFileTitle(templateName);
-    
+
     var possiblyChangedTitle = jQuery(titleSelector).val();
-    
+
     if (possiblyChangedTitle.trim() && possiblyChangedTitle !== proposedTitle) {
         proposedTitle = possiblyChangedTitle;
     }
-    
+
     // Put it in the correct directory.
     proposedTitle = "app/content/" + templateName + "/" + proposedTitle;
-    
+
     return proposedTitle;
 }
